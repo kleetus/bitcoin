dd5bd36be05a9f15c8dc59ecce992237c3298f8c
Chris Kleeschulte <chrisk@bitpay.com>
2016-11-07 17:53:27 -0500

    wip

--- src/wallet/exporter.c
+++ src/wallet/exporter.c
@@ -21,13 +21,13 @@ typedef struct {
 char *derivationMethods[] = { "SHA512", "scrypt" };
 
 const OBJ *objValues[6];
-OBJ type, id, ct, slt, dm, rnd, pk;   
+OBJ type, id, ct, slt, dm, rnd, pk;
 
 unsigned char ukeySerPK[429], pkHash[65], cipherText[97], salt[17], pubkey[67], strKey[5], logger[256];
 int ret, rounds = 0, nID = 0, ukeynum = 0, keynum = 0, ckeynum = 0, mkeynum = 0, recordsWritten = 0;
 short int silent = 0, valuesLen = 0, writeLog = 0, firstRecordWritten = 0;
 FILE *outfile;
-char *infilepath = NULL, *outfilepath = NULL; 
+char *infilepath = NULL, *outfilepath = NULL;
 uint32_t flags = DB_RDONLY;
 DB *db;
 DBC *cur;
@@ -78,14 +78,14 @@ int main(int argc, char *argv[]) {
   }
 
   db->cursor(db, NULL, &cur, 0);
-  
+
   //we zero out the key and value structs memset
   //there is a "size" and a "value", the size should be an int and the value should a void*
-  memset(&key, 0, sizeof(DBT)); 
-  memset(&value, 0, sizeof(DBT)); 
+  memset(&key, 0, sizeof(DBT));
+  memset(&value, 0, sizeof(DBT));
 
   if (outfilepath != NULL) {
-    outfile = fopen(outfilepath, "r+"); 
+    outfile = fopen(outfilepath, "r+");
     if (outfile != NULL) {
       fclose(outfile);
       char ans;
@@ -98,10 +98,10 @@ int main(int argc, char *argv[]) {
         outfile = fopen(outfilepath, "a");
       } else {
         fprintf(stdout, "Chose not to overwrite output file, exiting.");
-        return (0);  
+        return (0);
       }
     } //the file doesn't exist or we had an error trying to open it
-    else { 
+    else {
       outfile = fopen(outfilepath, "w");
       if (outfile == NULL) {
         fprintf(stderr, "Could not write to output file, exiting.");
@@ -113,14 +113,14 @@ int main(int argc, char *argv[]) {
   }
 
   printMetaInfo((const unsigned char*)"Searching the database for master keys and private keys...");
-  
+
   fprintf(outfile, "[\n  ");
-  /* 
+  /*
   note: the DBT objects for the key and the value are re-used. These DBT structures are
   auto initialized into the data segment of the binary. The smallest value for the data buffer is the
   page size for the database, 4K? This should be plenty large enough for all the data values that
-  we are interested in. If there isn't enough room in the buffer, then the DB_BUFFER_SMALL error will 
-  be returned. 
+  we are interested in. If there isn't enough room in the buffer, then the DB_BUFFER_SMALL error will
+  be returned.
   */
 
   while ((ret = cur->get(cur, &key, &value, DB_NEXT)) == 0) {
@@ -130,21 +130,21 @@ int main(int argc, char *argv[]) {
     }
 
     keynum++;
-    
+
     deserializeArray((const unsigned char*)key.data, 1, strKey, 0, 0, 1);
 
     if (strcmp((const char*)strKey, "mkey") == 0) {
       /* serialization format is:
-         
+
         key: [length][key name][id]
         example: [4][mkey][0]
 
-                  0       1..48       49   50..57   58..61     62..65      66 
+                  0       1..48       49   50..57   58..61     62..65      66
         value: [length][cipherText][length][salt][der method][der rounds][length][der params]
         example: [48][0..47][8][0..7][0][25000][0][0]
 
       */
-      valuesLen = 6; 
+      valuesLen = 6;
 
       type.type = 's';
       type.key = "type";
@@ -161,35 +161,35 @@ int main(int argc, char *argv[]) {
       ct.key = "cipherText";
       ct.type = 's';
       ct.data = cipherText;
-      objValues[2] = &ct; 
+      objValues[2] = &ct;
 
       deserializeArray((const unsigned char*)value.data, 50, salt, 8, 1, 1);
       slt.key = "salt";
       slt.type = 's';
       slt.data = salt;
-      objValues[3] = &slt; 
+      objValues[3] = &slt;
 
       int derInt = deserializeInt((const unsigned char*)value.data, 58);
       dm.key = "derivationMethod";
       dm.type = 's';
       dm.data = derivationMethods[derInt];
-      objValues[4] = &dm; 
+      objValues[4] = &dm;
 
       rounds = deserializeInt((const unsigned char*)value.data, 62);
       rnd.key = "rounds";
       rnd.type = 'i';
       rnd.data = &rounds;
-      objValues[5] = &rnd; 
+      objValues[5] = &rnd;
       mkeynum++;
       writeLog = 1;
 
     } else if (strcmp((const char*)strKey, "ckey") == 0) {
-      /* 
-         serialization format: 
-         key: [length][ckey][length][compressed pub key] 
-         example: [4][ckey][33][0..32] 
+      /*
+         serialization format:
+         key: [length][ckey][length][compressed pub key]
+         example: [4][ckey][33][0..32]
          value: [length][cipherText]
-         example: [48][0..47] <- 48 byts of data representing the 32 bytes plain text private key 
+         example: [48][0..47] <- 48 byts of data representing the 32 bytes plain text private key
       */
       ckeynum++;
       valuesLen = 3;
@@ -213,11 +213,11 @@ int main(int argc, char *argv[]) {
       writeLog = 1;
 
     } else if (strcmp((const char*)strKey, "key") == 0) { //this is an unencrypted key, the key is just "key" + compressed pub key
-      /* 
+      /*
          serialization format:
          key: [length][key][length][compressed pub key]
          example: [3][key][33][0..32]
-         value: [length][openssl evp serialized private key in der format][sha256 hash of private key + pub key] 
+         value: [length][openssl evp serialized private key in der format][sha256 hash of private key + pub key]
          example: [214][0..213][0..31] <-- 247 bytes total
       */
       ukeynum++;
@@ -240,14 +240,14 @@ int main(int argc, char *argv[]) {
       pk.data = pkHash;
       objValues[2] = &pk;
       writeLog = 1;
-      
+
     }
 
     if (writeLog) {
       if (firstRecordWritten) {
-        writeRecord(objValues, valuesLen, (const unsigned char*)",\n  ", outfile); 
+        writeRecord(objValues, valuesLen, (const unsigned char*)",\n  ", outfile);
       } else {
-        writeRecord(objValues, valuesLen, (const unsigned char*)"", outfile); 
+        writeRecord(objValues, valuesLen, (const unsigned char*)"", outfile);
         firstRecordWritten = 1;
       }
       recordsWritten++;
@@ -266,7 +266,7 @@ int main(int argc, char *argv[]) {
 
   sprintf((char*)logger, "Number of master keys found: %d.", mkeynum);
   printMetaInfo(logger);
-  
+
   sprintf((char*)logger, "Number of encrypted keys found: %d.", ckeynum);
   printMetaInfo(logger);
 
@@ -301,7 +301,7 @@ int usage(void) {
   fprintf(stderr, "\n\n");
   return (0);
 }
- 
+
 int testProvidedFiles(char *inFile, char *outFile) {
   if (inFile == NULL) {
     return (0);
@@ -309,7 +309,7 @@ int testProvidedFiles(char *inFile, char *outFile) {
   FILE *in, *out;
   in = fopen(inFile, "r");
   if (in == NULL) {
-    return (0); 
+    return (0);
   }
   fclose(in);
   if (outFile == NULL) {
@@ -320,7 +320,7 @@ int testProvidedFiles(char *inFile, char *outFile) {
     return (0);
   }
   fclose(out);
-  return (1); 
+  return (1);
 }
 
 int deserializeArray(const unsigned char *input, const unsigned int offset, unsigned char *output, unsigned int maxLen, const unsigned int outputHex, const unsigned int nullTerminate) {
@@ -344,17 +344,17 @@ int deserializeArray(const unsigned char *input, const unsigned int offset, unsi
     i++;
   }
   if (nullTerminate) {
-    output[i * (outputHex ? 2 : 1)] = '\0'; 
+    output[i * (outputHex ? 2 : 1)] = '\0';
   }
   return (maxLen);
 }
 
 //this assumes serialized little endian
 int deserializeInt(const char unsigned *input, const unsigned int start) {
-  return (uint32_t)((uint8_t)input[start] | 
-      ((uint8_t)input[start+1] << 8) | 
-      ((uint8_t)input[start+2] << 16) | 
-      ((uint8_t)input[start+3] << 24)); 
+  return (uint32_t)((uint8_t)input[start] |
+      ((uint8_t)input[start+1] << 8) |
+      ((uint8_t)input[start+2] << 16) |
+      ((uint8_t)input[start+3] << 24));
 }
 
 int writeRecord(const OBJ *values[], const unsigned int len, const unsigned char *prefix, FILE *file) {
@@ -365,19 +365,19 @@ int writeRecord(const OBJ *values[], const unsigned int len, const unsigned char
     char *linep = (i > 0 ? ",\n  " : "");
     switch (values[i]->type) {
       case 's':
-        fprintf(file, "%s  \"%s\": \"%s\"", linep, key, (char*)value); 
+        fprintf(file, "%s  \"%s\": \"%s\"", linep, key, (char*)value);
         break;
       case 'i':
-        fprintf(file, "%s  \"%s\": %d", linep, key, (*(int*)value)); 
+        fprintf(file, "%s  \"%s\": %d", linep, key, (*(int*)value));
         break;
       default:
         return (-1);
         break;
-    } 
+    }
   }
   fprintf(file, "\n  }");
   return (0);
-} 
+}
 
 void printMetaInfo(const unsigned char *message) {
   if (!silent) {
